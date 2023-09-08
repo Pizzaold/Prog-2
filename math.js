22c9021c3bfe2d12b14ece13eb286c6415c2315a
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-09-08 13:05:36 +0300

    math

---
+++ math.js
@@ -0,0 +1,13 @@
+const a = 100
+const b = 50
+let result = a + b
+
+result = Math.PI
+result = Math.round(2.4)
+result = Math.ceil(2.4)
+result = Math.floor(2.4)
+result = Math.sqrt(64)
+result = Math.abs(-3)
+result = Math.floor(Math.random() * 10)
+
+console.log(result)
