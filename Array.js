27061dfb75cfa191cd030a646d589ade3adb645b
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-09-08 13:43:23 +0300

    Array

--- Array.js
+++ Array.js
@@ -4,4 +4,10 @@ for(let i = 0; i < 6; i++) {
     array.push(Math.ceil(Math.random() * 100));
 }
 
+array.forEach((number) => {console.log(number)});
+
+array.sort((a, b) => {
+    return b - a;
+});
+
 console.log(array);
