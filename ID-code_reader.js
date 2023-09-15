89f7512d768f82a0524d089a4d63de29ae132e67
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2023-09-15 18:00:59 +0300

    Finall version of ID-code reader

--- ID-code_reader.js
+++ ID-code_reader.js
@@ -1,71 +1,75 @@
-const { register } = require('module');
+const prompt = require('prompt-sync')();
 
-const prompt = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-  
-prompt.question('You id code:', id => {
-    console.log(`Your id code is ${id}`);
-    if (id.length === 11) {
-        console.log('Your id code is correct');
-        const sex = (id) => {
-            if (id[0] % 2 === 0) {
-                return 'Female'
-            } else {
-                return 'Male'
-            } 
-        }
-        const birthDate = (id) => {
-            const censure = id[0];
-            const year = id.slice(1, 3);
-            const month = id.slice(3, 5);
-            const day = id.slice(5, 7);
-            let date = '';
-            switch (censure) {
-                case 1 || 2:
-                    date = `18${year}.${month}.${day}`;
-                    break;
-                case 3 || 4:
-                    date = `19${year}.${month}.${day}`;
-                    break;
-                case 5 || 6:
-                    date = `20${year}.${month}.${day}`;
-                    break;
-                default:
-                    date = `18${year}.${month}.${day}`;
-                    break;
-            }
-            return date;
-        }
-        const birthPlace = (id) => {
-            const register = id.slice(7, 10);
-            const places = {
-                e001 : 'Kuressaare haiglas',
-                e011 : 'Tartu Ülikooli Naistekliiniks',
-                e021 : 'Ida-Tallinna keskhaiglas \ Pelgulinna sünnitusmajas (Tallinn)',
-                e151 : 'Keila haiglas',
-                e161 : 'Rapla haiglas \ Loksa haiglas \ Hiiumaa haiglas (Kärdla)',
-                e221 : 'Ida-Viru keskhaiglas (Kohtla-Järve, endine Jõhvi)',
-                e271 : 'Maarjamõisa kliinikumis (Tartu) \ Jõgeva haiglas',
-                e371 : 'Narva haiglas',
-                e421 : 'Pärnu haiglas',
-                e471 : 'Haapsalu haiglas',
-                e491 : 'Järvamaa haiglas (Paide)',
-                e521 : 'Rakvere haiglas \ Tapa haiglas',
-                e571 : 'Valga haiglas',
-                e601 : 'Viljandi haiglas',
-                e651 : 'Lõuna-Eesti haiglas (Võru), Põlva haiglas ',
-            }
-            for (placeKey in places) {
-                if (register === placeKey) {
-                    return places[placeKey];
-                }
-            }
-        }
-        console.log(`You are ${sex(id)}, you where born on ${birthDate(id)}, you where born in `)
+const id = prompt('Sisesta oma isikukood: ');
+
+const sex = (id) => {
+    if (id[0] % 2 === 0) {
+        return 'Naine'
     } else {
-        console.log('Your id code is not correct');
+        return 'Mees'
+    } 
+}
+
+const birthDate = (id) => {
+    console.log(id);
+    const censure = id[0];
+    const year = id.slice(1, 3);
+    const month = id.slice(3, 5);
+    const day = id.slice(5, 7);
+    switch (Number(censure)) {
+        case 1 || 2:
+            return `${day}.${month}.18${year}`;
+        case 3 || 4:
+            return `${day}.${month}.19${year}`;
+        case 5 || 6:
+            return `${day}.${month}.20${year}`;
+        default:
+            return `${day}.${month}.18${year}`;
+
     }
-    prompt.close();
-});
+}
+
+const birthPlace = (id) => {
+    const register = Number(id.slice(7, 10));
+    switch (true) {
+        case register <= 10:
+            return 'Kuressaare haiglas';
+        case register <= 19:
+            return 'Tartu Ülikooli Naistekliiniks';
+        case register <= 150:
+            return 'Ida-Tallinna keskhaiglas või Pelgulinna sünnitusmajas (Tallinn)';
+        case register <= 160:
+            return 'Keila haiglas';
+        case register <= 220:
+            return 'Rapla haiglas või Loksa haiglas või Hiiumaa haiglas (Kärdla)';
+        case register <= 270:
+            return 'Ida-Viru keskhaiglas (Kohtla-Järve, endine Jõhvi)';
+        case register <= 370:
+            return 'Maarjamõisa kliinikumis (Tartu) või Jõgeva haiglas';
+        case register <= 420:
+            return 'Narva haiglas';
+        case register <= 470:
+            return 'Pärnu haiglas';
+        case register <= 490:
+            return 'Haapsalu haiglas';
+        case register <= 520:
+            return 'Järvamaa haiglas (Paide)';
+        case register <= 570:
+            return 'Rakvere haiglas või Tapa haiglas';
+        case register <= 600: 
+            return 'Valga haiglas';
+        case register <= 650:
+            return 'Viljandi haiglas';
+        case register <= 700:
+            return 'Lõuna-Eesti haiglas (Võru) või Põlva haiglas';
+    }
+}
+
+if (id.length === 11) {
+    console.log(`Sa oled ${sex(id)}.`)
+    console.log(`Sa sündisid ${birthDate(id)}.`)
+    console.log(`Sa olid ${id.slice(9, 10)}. inimene, kes sündis sel päeval ${birthPlace(id)}.`)
+    console.log(`Sinu kontrollnumber on ${id.slice(10, 11)}.`)
+} else {
+    console.log('Sinu sisestatud isikukood ei vasta nõuetel');
+}
